5e41c75a5748c56385eb0ad18c8d8f526e647567
Ronit <152080598+probablyronit@users.noreply.github.com>
2026-09-22 22:37:37 +0530

    Updated API_BASE

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 (() => {
   "use strict";
 
-  const API_BASE = "";
+    const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" 
+    ? "http://localhost:8000" 
+    : "";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
